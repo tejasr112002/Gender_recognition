73434781b734cf001ce89f91f344c060215eddec
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2022-10-30 16:09:32 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,13 @@
-import os
+"""
+Dataset preprocessing class
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
-import tensorflow as tf
 import random
+from sklearn.model_selection import train_test_split, KFold
+
 
 genders = ["m", "f"]
 ages = [
@@ -19,112 +22,131 @@ ages = [
 ]
 
 
-# read txt file with pandas
-def get_dataframe(path, frontal=False):
-    if frontal:
-        dfs = [
-            pd.read_csv(f"{path}/fold_frontal_{i}_data.txt", sep="\t") for i in range(5)
+# make Dataset preprocessing class
+class DatasetPreprocessing:
+    def __init__(self, path="data", frontal=False, n_max=None, new_size=None):
+        self.genders = ["m", "f"]
+        self.ages = [
+            "(0, 2)",
+            "(4, 6)",
+            "(8, 12)",
+            "(15, 20)",
+            "(25, 32)",
+            "(38, 43)",
+            "(48, 53)",
+            "(60, 100)",
         ]
-    else:
-        dfs = [pd.read_csv(f"{path}/fold_{i}_data.txt", sep="\t") for i in range(5)]
-    df = pd.concat(dfs, ignore_index=True)
+        self.new_size = new_size
+        self.n_max = n_max
+        self.frontal = frontal
+        self.path = path
+        self.df = self._get_preprocessed_dataframe()
+        self.dataset = self._dataframe_to_dataset(self.df)
 
-    # selct only the columns we need
-    image_path = (
-        df["user_id"].astype(str)
-        + "/"
-        + "landmark_aligned_face."
-        + df["face_id"].astype(str)
-        + "."
-        + df["original_image"].astype(str)
-    )
-    df.insert(0, "image_path", image_path)
-    # df = df[['image_path','age','gender']]
+    def _get_preprocessed_dataframe(self):
+        df = self._load_raw_data(self.path, self.frontal)
+        df = self._preprocess_dataframe(df)
+        # sample n_max rows
+        if self.n_max is not None:
+            df = df.sample(n=self.n_max)
+        return df
 
-    # drop rows with missing values
-    df = df.dropna()
+    def _load_raw_data(self, path="data", frontal=False):
+        if frontal:
+            dfs = [
+                pd.read_csv(f"{path}/fold_frontal_{i}_data.txt", sep="\t")
+                for i in range(5)
+            ]
+        else:
+            dfs = [pd.read_csv(f"{path}/fold_{i}_data.txt", sep="\t") for i in range(5)]
+        df = pd.concat(dfs, ignore_index=True)
+        return df
 
-    # drop rows with invalid genders
-    df = df[[x in ["m", "f"] for x in df.gender]]
+    def _preprocess_dataframe(self, df):
+        # add image paths
+        image_path = self._get_image_paths(df)
+        df.insert(0, "image_path", image_path)
+        # select only the columns we need
+        df = df[["image_path", "age", "gender"]]
 
-    # drop rows with invalid ages
-    df = df[[x in ages for x in df.age]]
+        # drop rows with missing values
+        df = df.dropna()
 
-    # convert age to the index in ages
-    df.age = df.age.apply(lambda x: ages.index(x))
-    # convert gender to the index in genders
-    df.gender = df.gender.apply(lambda x: genders.index(x))
+        # drop rows with invalid genders
+        df = df[[x in self.genders for x in df.gender]]
 
-    # shuffle the dataframe
-    df = df.sample(frac=1).reset_index(drop=True)
+        # drop rows with invalid ages
+        df = df[[x in self.ages for x in df.age]]
 
-    return df
+        # convert age and gender labels to indexes
+        df.age = df.age.apply(lambda x: ages.index(x))
+        df.gender = df.gender.apply(lambda x: genders.index(x))
 
+        return df
 
-def barplots_of_labels(df):
-    # make age and gender barplots in subplots
-    fig, ax = plt.subplots(2, 2, figsize=(10, 8))
-    df.age.value_counts().plot(kind="bar", ax=ax[0, 0])
-    df.gender.value_counts().plot(kind="bar", ax=ax[0, 1])
-    df.age[df.gender == 1].value_counts().plot(kind="bar", ax=ax[1, 0])
-    df.age[df.gender == 0].value_counts().plot(kind="bar", ax=ax[1, 1])
+    def _get_image_paths(self, df):
+        image_path = (
+            df["user_id"].astype(str)
+            + "/landmark_aligned_face."
+            + df["face_id"].astype(str)
+            + "."
+            + df["original_image"].astype(str)
+        )
+        return image_path
 
+    def _dataframe_to_dataset(self, df):
+        dataset = []
+        for row in df.itertuples():
+            # read image
+            image = cv2.imread(f"data/aligned/{row.image_path}")
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            if self.new_size is not None:
+                image = cv2.resize(image, self.new_size)
+            # extract gender and age from df and append tuple
+            dataset.append((image, row.gender, row.age))
+        # convert to data set to a tuple of numpy arrays
+        return tuple(map(np.array, zip(*dataset)))
 
-def create_dataset(df, n_max=False, new_size=False):
-    # create a dataset from a dataframe
-    dataset = []
-    N = n_max if n_max else len(df)
-    new_size = new_size if new_size else (224, 224)
-    for i in range(N):
-        # read image
-        image = cv2.imread(f"data/aligned/{df.image_path[i]}")
-        # resize image
-        image = cv2.resize(image, new_size)
-        # convert to RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # extract gender and age from df and append tuple
-        dataset.append((image, df.gender[i], df.age[i]))
-    return dataset
+    def _map_to_datasets(self, dfs):
+        return tuple(map(self._dataframe_to_dataset, dfs))
 
+    def get_age_label(self, age):
+        return self.ages[age]
 
-# function to split dataset
-def split_dataset(dataset, test_size=0.3):
-    # split dataset into train and test
-    random.shuffle(dataset)
-    N = len(dataset)
-    N_test = int(N * test_size)
-    N_train = N - N_test
-    train_dataset = dataset[:N_train]
-    test_dataset = dataset[N_train:]
-    # split train and test into X and y
-    X_train, gender_train, age_train = zip(*train_dataset)
-    X_test, gender_test, age_test = zip(*test_dataset)
-    # convert to numpy arrays
-    X_train = np.array(X_train)
-    gender_train = np.array(gender_train)
-    age_train = np.array(age_train)
-    X_test = np.array(X_test)
-    gender_test = np.array(gender_test)
-    age_test = np.array(age_test)
-    return X_train, gender_train, age_train, X_test, gender_test, age_test
+    def get_gender_label(self, gender):
+        return self.genders[gender]
 
+    def get_train_test_split(self, test_size=0.3, random_state=42):
+        train_df, test_df = train_test_split(
+            self.df, test_size=test_size, random_state=random_state
+        )
+        return self._map_to_datasets([train_df, test_df])
 
-def get_preprocessed_dataset(
-    path="data", frontal=False, n_max=False, new_size=False, test_size=0.3
-):
-    df = get_dataframe("data", frontal=frontal)
-    ds = create_dataset(df, n_max=n_max, new_size=new_size)
-    return split_dataset(ds, test_size=test_size)
+    def get_cv_splits(self, n_splits=5, random_state=42):
+        kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
+        # make a list of (train_df, test_df) tuples
+        cv_splits = [
+            self._map_to_datasets([self.df.iloc[train_index], self.df.iloc[test_index]])
+            for train_index, test_index in kf.split(self.df)
+        ]
+        return cv_splits
+
+    def get_classes(self):
+        return genders, ages
+
+    def get_dataset(self):
+        return self.dataset
+
+    def get_dataframe(self):
+        return self.df
 
 
 if __name__ == "__main__":
-    (
-        X_train,
-        gender_train,
-        age_train,
-        X_test,
-        gender_test,
-        age_test,
-    ) = get_preprocessed_dataset(
-        path="data", frontal=False, n_max=25, new_size=(256, 256), test_size=0.3
+    Data = DatasetPreprocessing(
+        path="data", frontal=True, n_max=25, new_size=(256, 256)
     )
+    # get train and test datasets [(X_train, y_train_gender, y_train_age), (X_test, y_test, y_train_gender, y_train_age)]
+    # train_test_ds = Data.get_train_test_datasets(test_size=0.3, random_state=42) # use for 70/30 split
+    train_test_ds = Data.get_cv_splits(
+        n_splits=5, random_state=42
+    )  # use for 5-fold cross validation
