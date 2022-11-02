8cef2c86766581329caaf9d8ffa96f662e0ecc00
Kasperjoergensen3 <Kasperjoergensen3@gmail.com>
2022-11-02 16:50:15 +0800

    trainer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from initialsation import *
 from preprocessing import DataPreprocessing
 from models import main_model, baseline_model, CNN_classic, CNN_transfer, CNN_multitask
+from trainer import Trainer
 
 # set seed for reproducibility
 set_seed(42)
@@ -9,14 +10,20 @@ set_seed(42)
 IMG_SIZE = 256
 
 preprocessing = DataPreprocessing(path="data", n_max=25, new_size=(IMG_SIZE, IMG_SIZE))
-datasets = preprocessing.get_cv_datasets()
+datasets = (
+    preprocessing.get_cv_datasets()
+)  # list of (X_train, (y_train_gender, y_train_age), X_test, (y_train_gender, y_train_age))
 
 models = {
-    "Main Model": (main_model(IMG_SIZE), None),
-    # "CNN classic": (CNN_classic(img_size), None),
-    # "CNN transfer": (CNN_transfer(img_size), None),
-    # "CNN multitask": (CNN_multitask(img_size), 0.5),
+    "Main Model": (main_model(IMG_SIZE), "gender", None),
+    # "CNN classic": (CNN_classic(img_size),"gender", None),
+    # "CNN transfer": (CNN_transfer(img_size),"gender", None),
+    # "CNN multitask": (CNN_multitask(img_size),"multi" 0.5),
 }
+
+trainer = Trainer(models, datasets, epochs=10)
+histories = trainer.train("Main Model")
+
 
 model = models["Main Model"][0]
 model.summary()
---
+++ trainer.py
@@ -0,0 +1,68 @@
+"""
+Helper functions
+"""
+from tensorflow import keras
+
+
+class Trainer:
+    def __init__(self, models, data, batch_size=50, no_epochs=10):
+        self.data = data
+        self.models = models
+        self.batch_size = batch_size
+        self.no_epochs = no_epochs
+        self.optimizer = "adam"
+        self.metrics = ["accuracy"]
+
+    def compile_model(self, model_name):
+        model = self.models[model_name][0]  # get model
+        target = self.models[model_name][1]  # get target
+        gamma = self.models[model_name][2]  # get gamma
+        if target == "multi":
+            loss = {
+                "gender": keras.losses.BinaryCrossentropy(),
+                "age": keras.losses.SparseCategoricalCrossentropy(),
+            }
+            loss_weights = {"gender": gamma, "age": 1 - gamma}
+            model.compile(
+                loss=loss,
+                loss_weights=loss_weights,
+                optimizer=self.optimizer,
+                metrics=self.metrics,
+            )
+        elif target == "gender":
+            model.compile(
+                loss=keras.losses.BinaryCrossentropy(),
+                optimizer=self.optimizer,
+                metrics=self.metrics,
+            )
+        elif target == "age":
+            model.compile(
+                loss=keras.losses.SparseCategoricalCrossentropy(),
+                optimizer=self.optimizer,
+                metrics=self.metrics,
+            )
+
+    def train_model(self, model_name):
+        self.compile_model(model_name)
+        model = self.models[model_name][0]  # get model
+        target = self.models[model_name][1]  # get target
+        histories = []
+        for data_split in self.data:
+            X_train, y_train, X_test, y_test = data_split
+            if target == "gender":
+                y_train = y_train[0]
+                y_test = y_test[0]
+            elif target == "age":
+                y_train = y_train[1]
+                y_test = y_test[1]
+            history = model.fit(
+                X_train,
+                y_train,
+                batch_size=self.batch_size,
+                use_multiprocessing=True,
+                epochs=self.no_epochs,
+                validation_data=(X_test, y_test),
+            )
+            histories.append(history)
+
+        return histories
